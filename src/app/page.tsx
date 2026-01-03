33526f6af0fd71cdf25b340c62a5335afef380a7
abdelrahman assem <abdelrahmanassem222@gmail.com>
2026-01-03 12:45:02 +0200

    modify:(add assets coingecko )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div className="text-3xl text-indigo-600">Home page</div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src={
+                "https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              }
+              alt="Bitcoin"
+              width={56}
+              height={56}
+            />
+
+            <div className="info">
+              <p>BitCoin / BTC</p>
+
+              <h1>&89,113.00</h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending Coins</p>
+      </section>
+
+      <section className="w-full wt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default page;
