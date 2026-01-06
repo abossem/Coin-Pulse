ce70157d077cddff8d27f38d628dc952bf83c9c2
abdelrahman assem <abdelrahmanassem222@gmail.com>
2026-01-06 09:23:10 +0200

    add:(separate components for fetching data in home page to use suspense around it)

---
+++ src/components/home/CoinOverview.tsx
@@ -0,0 +1,32 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import { formatCurrency } from "@/lib/utils";
+import Image from "next/image";
+
+export const CoinOverview = async () => {
+  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin");
+
+  return (
+    <div id="coin-overview">
+      <div className="header pt-2">
+        <Image
+          src={coin?.image?.large}
+          alt={coin?.name}
+          width={56}
+          height={56}
+        />
+
+        <div className="info">
+          <p>
+            {coin?.name} / {coin?.symbol.toUpperCase()}
+          </p>
+
+          <h1>
+            {coin?.market_data?.current_price?.usd
+              ? formatCurrency(coin.market_data.current_price.usd)
+              : "N/A"}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
