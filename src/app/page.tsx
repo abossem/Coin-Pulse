423f1ae8ad680822b58aba6192ce95316bdf43a6
abdelrahman assem <abdelrahmanassem222@gmail.com>
2026-01-07 09:31:23 +0200

    remove:(unused code )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,51 +6,6 @@ import {
 import { CoinOverview } from "../components/home/CoinOverview";
 import { TrendingCoins } from "../components/home/TrendingCoins";
 
-const dummyData: TrendingCoin[] = [
-  {
-    item: {
-      id: "bitcoin",
-      name: "Bitcoin",
-      symbol: "BTC",
-      market_cap_rank: 1,
-      thumb: "/assets/logo.svg",
-      large: "/assets/logo.svg",
-      data: {
-        price: 89113.0,
-        price_change_percentage_24h: { usd: 2.5 },
-      },
-    },
-  },
-  {
-    item: {
-      id: "ethereum",
-      name: "Ethereum",
-      symbol: "ETH",
-      market_cap_rank: 2,
-      thumb: "/assets/converter.svg",
-      large: "/assets/converter.svg",
-      data: {
-        price: 2500.0,
-        price_change_percentage_24h: { usd: -1.2 },
-      },
-    },
-  },
-  {
-    item: {
-      id: "solana",
-      name: "Solana",
-      symbol: "SOL",
-      market_cap_rank: 3,
-      thumb: "/assets/logo.svg",
-      large: "/assets/logo.svg",
-      data: {
-        price: 150.0,
-        price_change_percentage_24h: { usd: 5.0 },
-      },
-    },
-  },
-];
-
 const page = async () => {
   return (
     <main className="main-container">
